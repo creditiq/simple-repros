ab57d6126e4cd098276e4e49b236db7658c055b6
Sterling Camden <rusty.camden@gmail.com>
2021-04-14 17:26:18 -0700

    build(webpack): upgrade to 5

--- apps/main-app/webpack.config.js
+++ apps/main-app/webpack.config.js
@@ -12,13 +12,17 @@ const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
 const _ = require('lodash');
 
 const smp = new SpeedMeasurePlugin();
+
+const DEFAULT_GLOBAL_SCSS_PATHS = ['grid', 'global', 'toastr', 'piller'];
+module.exports.DEFAULT_GLOBAL_SCSS_PATHS = DEFAULT_GLOBAL_SCSS_PATHS;
+
 function createWebpackConfig({
   production,
   absoluteProjectRootPath,
   favicon,
   getApiStageVariables = () => ({}),
-  globalScssPaths = [],
-  extractCSS = production,
+  globalScssPaths = DEFAULT_GLOBAL_SCSS_PATHS,
+  extractCSS,
   devPort = 8000,
 }) {
   console.log('absolute path', absoluteProjectRootPath);
@@ -38,49 +42,51 @@ function createWebpackConfig({
   const localBackend = (!production && !!stageSettingsEnv.CIQ_LOCAL_BACKEND) || !!isApiStageLocal;
   const localSocket = !production && !!stageSettingsEnv.CIQ_LOCAL_SOCKET;
   const localCheckout = !production && !!stageSettingsEnv.CIQ_LOCAL_CHECKOUT;
-  const testProductionBuildWithDevServer = !!stageSettingsEnv.CIQ_TEST_PROD_BUILD || true;
+  const testProductionBuildWithDevServer = !!stageSettingsEnv.CIQ_TEST_PROD_BUILD;
   production = production || testProductionBuildWithDevServer;
+  extractCSS = extractCSS || !!production;
+
   const useDevServer = !production || testProductionBuildWithDevServer;
 
   const globalSassRegex = new RegExp(`(${globalScssPaths.join('|')})\\.scss$`);
   const useSourceMaps = false; // could make this !production if we want it in dev
-  const entry = './dist/app/index';
+  const entry = './dist/index';
 
-  const output = useDevServer
-    ? {
-        path: path.resolve(absoluteProjectRootPath + `/dist`),
-        publicPath: '/',
-        filename: 'bundle.js',
-      }
-    : {
-        path: path.resolve(absoluteProjectRootPath + `/dist/main`),
-        publicPath: '/',
-        filename: '[name].[chunkhash].js',
-        sourceMapFilename: '[file].map',
-      };
-
-  const definePluginGlobals = {
-    __DEV__: !production,
-    // auto stringify all strings, if for some horrible reason we wanted to insert a genuine code token we can override... but let's not do that
-    ..._.mapValues(getApiStageVariables(production, { apiStage, localBackend, localSocket, localCheckout }), (v) =>
-      _.isString(v) ? JSON.stringify(v) : v
-    ),
+  const output = {
+    path: path.resolve(absoluteProjectRootPath + `/dist/main`),
+    publicPath: '/',
+    filename: useDevServer ? '[name].bundle.js' : '[name].[contenthash].bundle.js',
   };
 
+  const apiStageVariables = getApiStageVariables(production, { apiStage, localBackend, localSocket, localCheckout });
+  const definePluginGlobals = _.mapValues(
+    {
+      __DEV__: !production,
+      'process.env.NODE_DEBUG': process.env.NODE_DEBUG,
+      'process.type': process.type,
+      'process.version': process.version,
+      ...apiStageVariables,
+    },
+    // auto stringify all strings, if for some horrible reason we wanted to insert a genuine code token we can override... but let's not do that
+    (v) => (_.isString(v) ? JSON.stringify(v) : v)
+  );
   const plugins = [
     // !production && new BundleAnalyzerPlugin() || (() => {}),
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     // new DuplicatePackageCheckerPlugin(),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^\.\/locale$/,
+      contextRegExp: /moment$/,
+    }),
 
     new webpack.DefinePlugin(definePluginGlobals),
 
     new MiniCssExtractPlugin({
-      filename: `[name].[contenthash].css`,
+      filename: useDevServer ? `[name].css` : `[name].[contenthash].css`,
     }),
     new HtmlWebpackPlugin({
       // Create HTML file that includes references to bundled CSS and JS.
       favicon,
-      template: '!!ejs-compiled-loader!src/app/index.ejs',
+      template: 'src/app/index.ejs',
       minify: {
         removeComments: true,
         collapseWhitespace: true,
@@ -98,14 +104,35 @@ function createWebpackConfig({
   ];
 
   const extractTextOptionsNonGlobal = [
-    !extractCSS ? 'style-loader' : MiniCssExtractPlugin.loader,
+    !extractCSS
+      ? {
+          loader: 'style-loader',
+          options: {
+            esModule: true,
+            modules: {
+              namedExport: true,
+            },
+          },
+        }
+      : {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            esModule: true,
+            modules: {
+              namedExport: true,
+            },
+          },
+        },
     {
       loader: 'css-loader',
       options: {
-        modules: true,
+        esModule: true,
+        modules: {
+          namedExport: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]',
+        },
         sourceMap: false,
         importLoaders: 1,
-        localIdentName: '[name]__[local]___[hash:base64:5]',
       },
     },
     {
@@ -140,13 +167,24 @@ function createWebpackConfig({
       modules: false,
       sourceMap: true,
       importLoaders: 1,
-      localIdentName: '[name]__[local]___[hash:base64:5]',
     },
   };
   extractTextOptionsCss.splice(extractTextOptionsCss.indexOf('sass-loader'), 1);
 
   const module = {
     rules: [
+      {
+        test: /\.ejs$/,
+        use: {
+          loader: 'ejs-compiled-loader',
+          options: {
+            htmlmin: true,
+            htmlminOptions: {
+              removeComments: true,
+            },
+          },
+        },
+      },
       // // commented out because it takes FOREVER in admin for some reason and we dont use source maps anyway
       // // maybe someday we will want them and debug the slowness
       ...(useSourceMaps
@@ -159,49 +197,19 @@ function createWebpackConfig({
             },
           ]
         : []),
-      // {
-      //   test: /\.tsx?$/,
-      //   use: [
-      //     {
-      //       loader: 'ts-loader',
-      //       options: {
-      //         compilerOptions: {
-      //           outDir: './',
-      //         },
-      //       },
-      //     },
-      //   ],
-      // },
-      ...[
-        { extension: 'eot' },
-        { extension: 'woff(2)?', mimetype: 'application/font-woff' },
-        { extension: 'ttf', mimetype: 'application/octet-stream' },
-        { extension: 'otf', mimetype: 'font/otf' },
-        { extension: 'svg', mimetype: 'image/svg+xml' },
-      ].map(({ extension, ...options }) => ({
-        test: new RegExp(`.${extension}(\\?v=\\d+.\\d+.\\d+)?$`),
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              fallback: require.resolve('file-loader'),
-              limit: 10000,
-              name: 'assets/fonts/[folder]/[name].[ext]',
-              ...options,
-            },
-          },
-        ],
-      })),
+      {
+        test: /\.(eot|woff2?|ttf|otf|svg)(\?v=\d+.\d+.\d+)?$/,
+        type: 'asset',
+        generator: {
+          filename: 'assets/fonts/[path][name][ext]',
+        },
+      },
       {
         test: /\.(jpe?g|png|gif|pdf|ico)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'assets/images/[name].[ext]',
-            },
-          },
-        ],
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name][ext]',
+        },
       },
       {
         test: (absPath) => /\.scss$/.test(absPath) && !globalSassRegex.test(absPath),
@@ -218,38 +226,52 @@ function createWebpackConfig({
     ],
   };
 
+  const mode = production ? 'production' : 'development';
   // webpack config object
   const config = {
-    mode: production ? 'production' : 'development',
+    mode,
     optimization: {
-      usedExports: true,
-      sideEffects: true, // this currently breaks i believe due to some circular dep thing
-      concatenateModules: !!production,
-      minimize: !!production,
-      // uncomment below to see symbols when in dev test mode
-      minimizer: [
-        new TerserPlugin({
-          parallel: true,
-          sourceMap: true, // Must be set to true if using source-maps in production
-          terserOptions: {
-            // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-            mangle: false,
-          },
-        }),
-      ],
+      runtimeChunk: true,
       splitChunks: {
+        chunks: 'all',
         cacheGroups: {
-          styles: {
-            name: 'styles',
-            test: /\.css$/,
-            chunks: 'all',
-            enforce: true,
+          defaultVendors: {
+            name: 'vendor',
+            // we don't have to test for @creditiq/ here because it resolves to a relative path and doesn't match anyway thanks to workspaces
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            reuseExistingChunk: true,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
           },
         },
       },
+      // comment below to test prod build with dev server with FULL minimization
+      minimizer: testProductionBuildWithDevServer
+        ? [
+            new TerserPlugin({
+              parallel: true,
+              terserOptions: {
+                // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+                mangle: false,
+              },
+            }),
+          ]
+        : undefined,
     },
+
     resolve: {
-      extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+      fallback: {
+        util: require.resolve('util/'),
+      },
+      extensions: ['.js', '.jsx', '.json'],
+      alias: {
+        '@src': path.resolve(absoluteProjectRootPath + '/dist'),
+        '@cmpts': path.resolve(absoluteProjectRootPath + '/dist/components'),
+      },
       symlinks: true,
     },
     entry,
@@ -262,15 +284,19 @@ function createWebpackConfig({
         ? 'source-map'
         : 'eval-source-map'
       : false,
+    resolveLoader: {
+      modules: ['node_modules', 'node_modules/@creditiq/rig-rig/node_modules'],
+    },
     performance: {
       hints: false,
     },
     devServer: useDevServer
       ? {
-          historyApiFallback: false,
-          hot: false,
-          overlay: false,
+          historyApiFallback: true,
+          hot: true,
+          overlay: true,
           port: devPort,
+          allowedHosts: ['.creditiq.com'],
         }
       : undefined,
   };
